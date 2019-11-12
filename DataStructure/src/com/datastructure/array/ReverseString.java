ed50cf1eff9f4f0b25675f19ddea004642d7b4d3
arasan21 <arasanbtech92@gmail.com>
2019-11-13 00:04:18 +0530

    Added description text

--- DataStructure/src/com/datastructure/array/ReverseString.java
+++ DataStructure/src/com/datastructure/array/ReverseString.java
@@ -1,21 +1,21 @@
-/**
- * 
- */
 package com.datastructure.array;
 
-import java.util.Arrays;
-
 /**
- * @author Poovarasan
- *
+ * Write a function that reverses a string. The input string is given as an array of characters 
+ * char[].Do not allocate extra space for another array, you must do this by modifying the 
+ * input array in-place with O(1) extra memory.
  */
 public class ReverseString {
 	
 	/**
-	 * @param s
-	 * @param leftIndex
-	 * @param rightIndex
-	 * @return
+	 * recursiveReverseString method reverses the char array using recursion
+	 * Time complexity -> O(n) - since it has to loop to each element
+	 * Space complexity -> O(n) due to recursion as it takes space to keep 
+	 * track of the function calls
+	 * @param s - input char array
+	 * @param leftIndex - starting index
+	 * @param rightIndex - ending index
+	 * @return reversed char array
 	 */
 	public char[] recursiveReverseString(char[] s, int leftIndex, int rightIndex) {
 	    if (leftIndex >= rightIndex) 
@@ -26,9 +26,12 @@ public class ReverseString {
 	    return recursiveReverseString(s, ++leftIndex, --rightIndex);
 	  }
 	
-	  /**
-	 * @param s
-	 * @return
+	/**
+	 * reverseString method uses loop to reverse the given char array
+	 * Time complexity -> O(n) - since it has to loop to each element
+	 * Space complexity -> O(1)
+	 * @param s - input char array
+	 * @return reversed char array
 	 */
 	public char[] reverseString(char[] s) {
 	    int leftIndex = 0, rightIndex= s.length-1;
