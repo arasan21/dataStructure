939521eee003b59149710bc91d39ef7f7bab6791
arasan21 <arasanbtech92@gmail.com>
2019-11-18 23:45:27 +0530

    Added MaxDepthBTree file

---
+++ DataStructure/src/com/datastructure/tree/MaxDepthBTree.java
@@ -0,0 +1,18 @@
+package com.datastructure.tree;
+
+public class MaxDepthBTree {
+
+	public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
